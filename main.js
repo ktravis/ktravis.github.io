4a49799f231a62b9b9ec2fa2cbaf26abd953aae3
Kyle Travis <wetscience@gmail.com>
2013-10-17 18:16:27 -0700

    revamped projects page and images

--- main.js
+++ main.js
@@ -7,23 +7,20 @@ var points = { "projects" : 20,
 var sections = {
 "projects" :'<div class="content-body" id="projects">'
         +   '   <h1>Projects</h1>'
-        +   '   <hr>'
-        +   '   <p><a href="//www.kongregate.com/games/kmakai/isolation">ISOLATION</a> ( a game about personal space )</p>'
-        +   '   <p><a href="//ktravis.github.io/yt-freedom">YOUTUBE FREEDOM</a> ( minimalist viewing window for youtube )</p>'
-        +   '   <p><a href="//tinyforum.herokuapp.com">TINYFORUM</a> ( a small bulletin board system, written in clojure )</p>'
-        +   '   <p>HUSK ( interactive fiction game and platform )</p>'
-        +   '   <p><a href="//gist.github.com/ktravis">SCRAPS</a> ( minor works... )</p>'
+        +   '   <p class="project" id="isolation"><span><a href="//www.kongregate.com/games/kmakai/isolation">ISOLATION</a> ( a game about personal space )</span></p>'
+        +   '   <p class="project" id="freedom"><span><a href="//ktravis.github.io/yt-freedom">YOUTUBE FREEDOM</a> ( minimalist viewing window for youtube )</span></p>'
+        +   '   <p class="project" id="tinyforum"><span><a href="//tinyforum.herokuapp.com">TINYFORUM</a> ( a small bulletin board system, written in clojure )<span></p>'
+        +   '   <p class="project" id="husk"><span>HUSK ( interactive fiction game and platform )<span></p>'
+        +   '   <p class="project" id="scraps"><span><a href="//gist.github.com/ktravis">SCRAPS</a> ( minor works... )<span></p>'
         +   '</div>',
 
 "blog" :    '<div class="content-body" id="blog">'
         +   '   <h1>Blog</h1>'
-        +   '   <hr>'
         +   '   <p>coming soon...</p>'
         +   '</div>',
 
 "about" :   '<div class="content-body" id="about">'
         +   '   <h1>About</h1>'
-        +   '   <hr>'
         +   '   <p><pre>ktravis = {\n\t"name"       : "Kyle Travis",\n\t"location"   : "Eugene, OR",\n\t"occupation" : "Student",\n\t"studying"   : ("Math", "Physics"),\n\t"email"      : "<a href="mailto:ktravis@uoregon.edu">ktravis@uoregon.edu</a>",\n\t"twitter"    : "<a href="//www.twitter.com/kmakai">@kmakai</a>",\n\t"github"     : "<a href="//github.com/ktravis">github://ktravis</a>"\n}</pre></p>'
         +   '   <div id="about-more"> . . . </div>'
         +   '   <div id="about-hidden">Kyle Travis is a student of Applied Math and Physics at the University of Oregon, where he works part-time as a research assistant creating analysis software. Kyle develops small <a href="//kongregate.com/accounts/kmakai">games</a> and web applications when not arguing with compilers or scanning javadocs. <br><br>Languages and tools of choice:<ul><li>python</li><li>java</li><li>javascript</li><li>clojure</li><li>openGL</li><li>vim</li><li>archlinux</li><li>github</li></ul></div>'
